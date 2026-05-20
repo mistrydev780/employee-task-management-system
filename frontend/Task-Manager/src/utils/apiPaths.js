496425d8eb0b6271f9a019f9549d377a2298d59b
Dev Jangid <devjangid788@gmail.com>
2026-05-20 12:07:09 +0530

    updated production api url

--- frontend/Task-Manager/src/utils/apiPaths.js
+++ frontend/Task-Manager/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://employee-task-management-system-iryh.onrender.com";
 // utils/apiPaths.js
 export const API_PATHS = {
     AUTH: {
